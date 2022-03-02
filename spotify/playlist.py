ffb7b7495b7838927ffc567a36e7cbd7435d0b07
MattPChoy <matthewpchoy@gmail.com>
2022-03-02 14:24:59 +1000

    Added capability to generate songs objects within playlist

--- spotify/playlist.py
+++ spotify/playlist.py
@@ -1,7 +1,15 @@
 from .spotify import Spotify # Class import
+from .song import Song # Class import
 
 class Playlist:
     def __init__(self, playlist_id, spotify):
+        """
+        Create an instance of the Playlist class that contains information
+        parsed from the web API (which returns a JSON object.)
+
+        @param playlist_id: Spotify's unique identifier for a playlist
+        @param spotify: Spotify object, which contains auth information
+        """
         self.spotify = spotify
         self.playlist_id = playlist_id
 
@@ -9,6 +17,9 @@ class Playlist:
         self.__parse_json_data__()
 
     def __get_json_data__(self):
+        """
+        Obtain the JSON data from the web
+        """
         assert self.spotify is not None, "Spotify object not defined (is None)"
         assert self.playlist_id is not None, "Playlist ID is None"
 
@@ -17,9 +28,19 @@ class Playlist:
             playlist_id=self.playlist_id
         )
 
+    def __parse_playlist_songs__(self):
+        self.json_tracks = self.json['tracks']['items']
+        for track in self.json_tracks:
+            self.songs.append(Song(json=track))
+
     def __parse_json_data__(self):
         self.collaborative = self.json['collaborative']
         self.description = self.json['description']
         self.num_followers = self.json['followers']['total']
         self.name = self.json['name']
-        pass
+        # self.owner =
+        self.songs = []
+        self.__parse_playlist_songs__() # This updates self.songs
+
+    def __str__(self):
+        return f""
--- spotify/song.py
+++ spotify/song.py
@@ -1,3 +1,15 @@
 class Song():
     def __init__(self, json):
-        return None
+        self.added_at = json['added_at']
+        # self.added_by = User(json['added_by'])
+        self.artists = []
+        # self.artists = [] using json['track'][artists]
+
+        # Track details
+        self.duration_ms = json['track']['duration_ms']
+        self.explicit = json['track']['explicit']
+        self.name = json['track']['name']
+        self.id = json['track']['id']
+
+    def __str__(self):
+        return f"{self.name} by {self.artists} ({self.id})"
